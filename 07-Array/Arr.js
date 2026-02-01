2eb187cfaa1e49dcc54c2a9b6648198465c4e747
gaurav <gouravkumar42242@gmail.com>
2026-02-01 13:50:34 +0530

    comment change

--- 07-Array/Arr.js
+++ 07-Array/Arr.js
@@ -1,50 +1,50 @@
 // Array // 
 
-// const arr = [1,2,3,4,5,6,7]; 
-// const arr2 = ["gaurav" , "Rohit" , "Rohan"]; 
+const arr = [1,2,3,4,5,6,7]; 
+const arr2 = ["gaurav" , "Rohit" , "Rohan"]; 
 
-// // js can hold all the value like string , int and bool in one arr
-// const arr3 = [1,"gaurav" , true]; 
+// js can hold all the value like string , int and bool in one arr
+const arr3 = [1,"gaurav" , true]; 
 
-// console.log(arr3[2]); //true 
-// console.log(arr2.length); //3 
-// console.log(arr2.indexOf("gaurav")); //0 
-
-
-// // push method // 
-// const arr4 = [] 
-// arr4.push("gaurav" , 19 , true); 
-// console.log(arr4); // [ 'gaurav', 19, true ]
-
-// // pop method // 
-// arr4.pop(); 
-// console.log(arr4); // [ 'gaurav', 19 ] -> it will pop the last element. 
+console.log(arr3[2]); //true 
+console.log(arr2.length); //3 
+console.log(arr2.indexOf("gaurav")); //0 
 
 
-// // .unshift(item1, item2, ...): Adds one or more items to the beginning. // 
-// let arr5 = ["user2" , "user3"]; 
-// arr5.unshift("user1"); 
-// console.log((arr5)); // [ 'user1', 'user2', 'user3' ]
+// push method // 
+const arr4 = [] 
+arr4.push("gaurav" , 19 , true); 
+console.log(arr4); // [ 'gaurav', 19, true ]
 
-// // .shift() --> remove the first element. 
-// arr5.shift(); 
-// console.log(arr5); // [ 'user2', 'user3' ]
+// pop method // 
+arr4.pop(); 
+console.log(arr4); // [ 'gaurav', 19 ] -> it will pop the last element. 
+
+
+// .unshift(item1, item2, ...): Adds one or more items to the beginning. // 
+let arr5 = ["user2" , "user3"]; 
+arr5.unshift("user1"); 
+console.log((arr5)); // [ 'user1', 'user2', 'user3' ]
+
+// .shift() --> remove the first element. 
+arr5.shift(); 
+console.log(arr5); // [ 'user2', 'user3' ]
 
 
 
 // looping of arr  //
 
-// let price = [20,22,34,45,66,554,43]; 
+let price = [20,22,34,45,66,554,43]; 
 
-// for(let i=0; i<price.length; i++){
-//     console.log(price[i]); // 20,22,34,45,66,554,43
-// }
+for(let i=0; i<price.length; i++){
+    console.log(price[i]); // 20,22,34,45,66,554,43
+}
 
 
-// // for of loop //
-// for(const cost of price){
-//     console.log(cost); // 20,22,34,45,66,554,43
-// }
+// for of loop //
+for(const cost of price){
+    console.log(cost); // 20,22,34,45,66,554,43
+}
 
 
 // Advanced Array Manipulation // 
